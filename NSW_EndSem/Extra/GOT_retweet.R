791351c3a74fb43c0ff41daed29cef61f823f42b
shubh1410 <shubhamdarokar1410@gmail.com>
2019-12-04 08:38:15 +0530

    final commit

---
+++ NSW_EndSem/Extra/GOT_retweet.R
@@ -0,0 +1,14 @@
+  library("igraph")
+  links <- read.table("/home/shuham/Desktop/Projects/NSW/NSW_Kashmir/retweet_GOT.txt")
+  head(links)
+  typeof(links)
+  links[1:1,]
+    kashmir_retweet <- graph_from_data_frame(d=links,directed = T)
+  
+  tkplot(kashmir_retweet,layout=layout.auto(kashmir_retweet),edge.arrow.size=0.6,edge.color="black",vertex.color="orange",vertex.size=15, 
+         
+         vertex.frame.color="black", vertex.label.color="black", 
+         
+         vertex.label.cex=0.5, vertex.label.dist=0)
+  
+    
---
+++ NSW_EndSem/Extra/kashmir_retweet.R
@@ -0,0 +1,12 @@
+    library("igraph")
+    links <- read.table("/home/shuham/Desktop/retweet.txt")
+    head(links)
+    kashmir_retweet <- graph_from_data_frame(d=links,directed = T)
+    
+    tkplot(kashmir_retweet,layout=layout.auto(kashmir_retweet),edge.arrow.size=0.6,edge.color="black",vertex.color="orange",vertex.size=15, 
+         
+         vertex.frame.color="black", vertex.label.color="black", 
+         
+         vertex.label.cex=0.6, vertex.label.dist=0)
+    
+      
